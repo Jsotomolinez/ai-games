976ccba5ac6d3db0291e6133e276019996a1194c
jsotomolinez <jsotomolinez12@gmail.com>
2025-10-15 23:02:16 -0400

    "cambio de el header a español"

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full bg-primary shadow-lg mb-5">
+    <header className="w-full bg-primary shadow-lg mb-5 p-1">
       <div className="container mx-auto flex h-16 items-center justify-center px-4">
         <Link href="/">
-          <h1>AI Games</h1>
+          <h1>humano vs IA 🤖</h1>
         </Link>
       </div>
     </header>
